952b63b2246abbe0591788be7660a19c04612457
FelixCodesTech <66774630+FelixCodesTech@users.noreply.github.com>
2023-03-11 09:40:45 +0100

    Finished game

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
 correctInput = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 playerInput = str(input("Write the entire ABC here: ")).lower()
+success = 0
+failed = 0
 
 
 # loop through the string
 for pos, char in enumerate(playerInput):
-  # print each character
-  print(char)
+  if char == correctInput[pos]:
+    success += 1
+  else:
+    failed += 1
+
+print(f"You entered: {playerInput}. Out of {len(correctInput)} characters you got {success} right and {failed} wrong. That's {round(success/len(correctInput)*100)}%")
