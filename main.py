2e383beb29e572f4571fb72544f0cc80a432fb14
FelixCodesTech <66774630+FelixCodesTech@users.noreply.github.com>
2023-03-11 09:50:47 +0100

    Added timer function

--- main.py
+++ main.py
@@ -1,14 +1,24 @@
+import time as t
 correctInput = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
-playerInput = str(input("Write the entire ABC here: ")).lower()
-success = 0
-failed = 0
 
 
-# loop through the string
-for pos, char in enumerate(playerInput):
-  if char == correctInput[pos]:
-    success += 1
-  else:
-    failed += 1
+while True:
+  t1 = t.time()
+  playerInput = str(input("Write the entire ABC here: ")).lower()[:len(correctInput)]
+  t2 = t.time()
+  timeNeeded = round(t2-t1, 1)
+  success = 0
+  failed = 0
 
-print(f"You entered: {playerInput}. Out of {len(correctInput)} characters you got {success} right and {failed} wrong. That's {round(success/len(correctInput)*100)}%")
+  # loop through the string
+  for pos, char in enumerate(playerInput):
+    if char == correctInput[pos]:
+      success += 1
+    else:
+      failed += 1
+
+  print(f"You entered: {playerInput} in {timeNeeded}s. Out of {len(correctInput)} characters you got {success} right and {failed} wrong. That's {round(success/len(correctInput)*100)}%")
+
+
+  if input("Wanna play again [enter/n]: ") == "n":
+    break
